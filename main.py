b492a1e33368983caedcd2c06135926b0c960a58
Marin <blackrossiso@gmail.com>
2017-02-06 07:40:25 +0100

    functions

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
+def findCandidate():
+    return ""
 
+def displayCandidates():
+    return ""
 
-visited = {}
-for line in open('1qrs.pdb'):
-    list = line.split()
-    id = list[0]
-    if id == 'ATOM':
-        type = list[2]
-        if type == 'CA':
-            residue = list[3]
-            type_of_chain = list[4]
-            atom_count = int(list[5])
-            position = list[6:8]
-            if atom_count >= 0:
-                if type_of_chain not in visited:
-                    visited[type_of_chain] = 1
-                    print residue,type_of_chain,atom_count,' '.join(position)
+def rankCandidates():
+    return ""
+
+def FR3DSearch():
+    return ""
+
+def AddFiletoSearch():
+    return ""
+
+def getAtom(file, number):
+    visited = {}
+    for line in open(file):
+        list = line.split()
+        type = list[0]
+        if type == 'ATOM':
+            # type = list[2]
+            id = list[1]
+            # print number
+            if id == str(number):
+                # print "gfssgf"
+                print line
+            # else:
+                # print "lipa"
+            # if type == 'CA':
+            #     residue = list[3]
+            #     type_of_chain = list[4]
+            #     atom_count = int(list[5])
+            #     position = list[6:8]
+            #     if atom_count >= 0:
+            #         if type_of_chain not in visited:
+            #             visited[type_of_chain] = 1
+            #             print residue,type_of_chain,atom_count,' '.join(position)
+
+print "hge"
+getAtom('1rc7.pdb', 977)
