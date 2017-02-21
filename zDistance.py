d422069ef6f9320a0c9ae2abaca25cf4e9e487b5
Marin <blackrossiso@gmail.com>
2017-02-21 07:43:19 +0100

    functions

--- zDistance.py
+++ zDistance.py
@@ -2,19 +2,27 @@ import numpy as np
 
 
 def zDistance(A,B):
+
+
     [M, t] = (A.size);
-    [N, s] = (B.size);
+    if B == False:
+        G = np.matmul(A, np.transpose(A))
+        a = np.diag(G);
+        X = np.repeat(a, 1, M);
+        D = np.sqrt(X + np.transpose(X) - 2*G);            # |u-v| = |u|^2 + |v|^2 - 2 u . v
+    else:
+        [N, s] = (B.size);
 
-    a = sum(np.multiply(A,A))
-    b = sum(np.multiply(B,B))
+        a = sum(np.multiply(A,A))
+        b = sum(np.multiply(B,B))
 
-    if M < 150:
-        X =  np.matmul(np.transpose(a) , np.ones(1,N));
-        Y = np.ones(M, 1) * b;
-    else
-        X = np.repeat(b, M, 1);
-        Y = np.repeat(np.transpose(a),1,N);
+        if M < 150:
+            X =  np.matmul(np.transpose(a) , np.ones(1,N));
+            Y = np.ones(M, 1) * b;
+        else:
+            X = np.repeat(b, M, 1);
+            Y = np.repeat(np.transpose(a),1,N);
 
-    D = np.sqrt(X + Y - 2 * A * np.transpose(B));
+            D = np.sqrt(X + Y - 2 * A * np.transpose(B));
 
     return D
